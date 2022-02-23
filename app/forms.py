c78fce23aea89213494286c5a0f732252066559c
Raffique <raffiquemuir@yahoo.com>
2022-02-23 12:18:33 -0500

    50% of lab 4

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import FileField
+from flask_wtf.file import FileRequired, FileAllowed
+
+image_types = ['jpg', 'png', 'jpeg', 'gif', 'svg']
+
+class UploadForm(FlaskForm):
+    file = FileField('File', validators=[FileAllowed(image_types, 'Image only!'), FileRequired('File was empty!')])
